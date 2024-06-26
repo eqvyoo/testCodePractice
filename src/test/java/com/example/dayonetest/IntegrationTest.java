774ab812dd8c805e97e1f8cc60ad9e7d6c54ea45
eqvyoo <ys07106@gmail.com>
2024-06-27 00:00:43 +0900

    AWS S3 테스트 환경 구축 / 테스트 코드 작성

--- src/test/java/com/example/dayonetest/IntegrationTest.java
+++ src/test/java/com/example/dayonetest/IntegrationTest.java
@@ -6,10 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.io.File;
@@ -26,6 +26,8 @@ public class IntegrationTest {
     static DockerComposeContainer rdbms;
 
     static RedisContainer redis;
+
+    static LocalStackContainer aws;
 
     static {
         rdbms = new DockerComposeContainer(new File("infra/test/docker-compose.yaml"))
@@ -44,6 +46,10 @@ public class IntegrationTest {
 
         redis = new RedisContainer(RedisContainer.DEFAULT_IMAGE_NAME.withTag("6"));
         redis.start();
+        aws = (new LocalStackContainer())
+                .withServices(LocalStackContainer.Service.S3)
+                .withStartupTimeout(Duration.ofSeconds(600));
+        aws.start();
     }
 
     static class IntegrationTestInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext>{
@@ -61,6 +67,19 @@ public class IntegrationTest {
             properties.put("spring.datasource.url","jdbc:mysql://" + rdbmsHost + ":" + rdbmsPort+ "/score");
             properties.put("spring.data.redis.host", redisHost);
             properties.put("spring.data.redis.port", redisPort.toString());
+
+            try{
+                aws.execInContainer(
+                        "awslocal",
+                        "s3api",
+                        "create-bucket",
+                        "--bucket",
+                        "test-bucket"
+                );
+                properties.put("aws.endpoint", aws.getEndpoint().toString());
+            } catch (Exception e){
+                // ignore
+            }
 
             TestPropertyValues.of(properties)
                     .applyTo(applicationContext);
