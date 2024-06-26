0c24361ee0eef87d76a8becbbbef25e0e4bbbf9f
eqvyoo <ys07106@gmail.com>
2024-06-27 00:53:15 +0900

    Kafka 테스트 환경 구축 / 테스트 코드 작성

---
+++ src/main/java/com/example/dayonetest/KafkaConsumerApplication.java
@@ -0,0 +1,27 @@
+package com.example.dayonetest;
+
+import com.example.dayonetest.service.KafkaConsumerService;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaConsumerApplication {
+    private final KafkaConsumerService kafkaConsumerService;
+
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name("test-topic")
+                .build();
+    }
+
+    @KafkaListener(id = "test-id", topics = "test-topic")
+    public void listen (String message){
+        kafkaConsumerService.process(message);
+    }
+
+}
